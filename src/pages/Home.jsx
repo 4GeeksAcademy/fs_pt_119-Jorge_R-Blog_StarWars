f0c5ea1235529d1d24fd9a804f1a3a8fb66f67aa
JorgeCKR <jorgeckr@gmail.com>
2025-10-12 19:57:06 +0000

    Mapeo completado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { Link } from "react-router-dom";
@@ -6,10 +7,51 @@ export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
+	async function obtenerListas() {
+		try {
+			const [respPeople, respStarships, respPlanets] = await Promise.all([
+				fetch("https://www.swapi.tech/api/people"),
+				fetch("https://www.swapi.tech/api/starships"),
+				fetch("https://www.swapi.tech/api/planets"),
+			]);
+
+			if (!respPeople.ok || !respStarships.ok || !respPlanets.ok) {
+				throw new Error("Error al cargar los datos. Contacte a soporte técnico XD");
+			}
+
+			const [dataPeople, dataStarships, dataPlanets] = await Promise.all([
+				respPeople.json(),
+				respStarships.json(),
+				respPlanets.json(),
+			]);
+
+			console.log("Personajes:", dataPeople);
+			console.log("Naves:", dataStarships);
+			console.log("Planetas:", dataPlanets);
+
+			dispatch({
+				type: "set_listas",
+				payload: {
+					people: dataPeople.results,
+					starships: dataStarships.results,
+					planets: dataPlanets.results,
+				},
+			});
+
+		} catch (error) {
+			console.error("Error al obtener las listas:", error);
+		}
+	}
+
+	useEffect(() => {
+		obtenerListas();
+	}, []);
 	return (
+		
 		<div className="container">
 			<h1>Personajes</h1>
 			<div className="d-flex flex-nowrap overflow-auto">
+				{store.listas.people.map((item) => (
 				<div className="contenedor card me-3">
 					<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/characters/1.jpg" className="card-img-top" alt="..." />
 					<div className="card-body">
@@ -17,7 +59,7 @@ export const Home = () => {
 						<p className="card-text">Some quick example text...</p>
 						<div className="d-flex justify-content-between align-items-center">
 							<Link className="dropdown-item" to="/sabermas">
-							<button type="button" className="btn btn-outline-primary">Saber mas...</button>
+								<button type="button" className="btn btn-outline-primary">Saber mas...</button>
 							</Link>
 							<button type="button" className="btn btn-outline-warning btn-sm">
 								<i className="fa-regular fa-heart btn"></i>
@@ -25,43 +67,8 @@ export const Home = () => {
 						</div>
 					</div>
 				</div>
-
-				<div className="contenedor card me-3">
-					<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/characters/8.jpg" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text...</p>
-						<button type="button" className="btn btn-primary">Saber mas...</button>
-					</div>
-				</div>
-
-				<div className="contenedor card me-3">
-					<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/characters/9.jpg" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text...</p>
-						<button type="button" className="btn btn-primary">Saber mas...</button>
-					</div>
-				</div>
-
-				<div className="contenedor card me-3">
-					<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/characters/2.jpg" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text...</p>
-						<button type="button" className="btn btn-primary">Saber mas...</button>
-					</div>
-				</div>
-
-				<div className="contenedor card me-3">
-					<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/characters/10.jpg" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text...</p>
-						<button type="button" className="btn btn-primary">Saber mas...</button>
-					</div>
-				</div>
+				))}
 			</div>
-		</div>
+		</div>	
 	);
 }; 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
     todos: [
       {
@@ -12,21 +12,32 @@ export const initialStore=()=>{
         title: "Do my homework",
         background: null,
       }
-    ]
+    ],
+    listas: {
+      people: [{}],
+      starships: [{}],
+      planets: [{}]
+    }
   }
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
+  switch (action.type) {
     case 'add_task':
 
-      const { id,  color } = action.payload
+      const { id, color } = action.payload
 
       return {
         ...store,
         todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
       };
+
+    case 'set_listas':
+      return {
+        ...store,
+        listas: action.payload
+      };
     default:
       throw Error('Unknown action.');
-  }    
+  }
 }
